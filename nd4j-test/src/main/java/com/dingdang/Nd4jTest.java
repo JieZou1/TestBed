77ea8807674ec76cc30a77134407bab112e1fa69
Jie Zou <zouj.rpi@gmail.com>
2016-08-23 17:58:18 -0400

    add nd4j-test

---
+++ nd4j-test/src/main/java/com/dingdang/Nd4jTest.java
@@ -0,0 +1,13 @@
+package com.dingdang;
+
+/**
+ * Hello world!
+ *
+ */
+public class Nd4jTest
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
