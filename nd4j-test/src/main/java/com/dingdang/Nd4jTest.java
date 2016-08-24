3489a920d1aeb569c6369de6218b60b858c4a383
Jie Zou <zouj.rpi@gmail.com>
2016-08-24 14:57:37 -0400

    add ND4J dependency

--- nd4j-test/src/main/java/com/dingdang/Nd4jTest.java
+++ nd4j-test/src/main/java/com/dingdang/Nd4jTest.java
@@ -8,6 +8,6 @@ public class Nd4jTest
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Start ND4J Testing!" );
     }
 }
