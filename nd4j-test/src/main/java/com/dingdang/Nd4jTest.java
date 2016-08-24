92afdb08691d3f4874380a69e94d9cb9f5bd0c79
Jie Zou <zouj.rpi@gmail.com>
2016-08-24 17:17:16 -0400

    testing ND4J CPU and GPU

--- nd4j-test/src/main/java/com/dingdang/Nd4jTest.java
+++ nd4j-test/src/main/java/com/dingdang/Nd4jTest.java
@@ -1,5 +1,8 @@
 package com.dingdang;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,8 @@ public class Nd4jTest
     public static void main( String[] args )
     {
         System.out.println( "Start ND4J Testing!" );
+
+        INDArray nd = Nd4j.create(new float[]{1,2,3,4,5,6},new int[]{3,2}, 'c');
+        System.out.println(nd);
     }
 }
